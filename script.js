30674e898cee79e7ced2c56dc43b8bf7f86e9338
JollyCoder <167334377+YoucefJC@users.noreply.github.com>
2024-07-27 15:04:00 +0200

    Change from a row approach to a box only approach
    Remove the previous implementation using rows and boxes per rows and
    use boxes only that are arranged programmatically in a square grid using
    flexbox and minimum widths and heights

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
+const CONTAINER_WIDTH = '960';
+const NUM_BOX_PER_SIDE = 16;
+const NUM_BOXES = NUM_BOX_PER_SIDE ** 2;
+const BOX_WIDTH = Math.floor(CONTAINER_WIDTH / NUM_BOX_PER_SIDE) + 'px';
+
 let grid16 = document.querySelector('.grid-16-layout');
+grid16.style.maxWidth = CONTAINER_WIDTH + 'px';
 
-for (let rowIndex = 0; rowIndex < 16; rowIndex++) {
-
-    let gridRow = document.createElement('div');
-    gridRow.classList.add('grid-row');
-    grid16.appendChild(gridRow);
-
-    for (let colIndex = 0; colIndex < 16; colIndex++) {
-
-        let gridBox = document.createElement('div');
-        gridBox.classList.add('grid-box');
-        gridRow.appendChild(gridBox);   
-    }
+for (let Index = 0; Index < NUM_BOXES; Index++) {
+    
+    let gridBox = document.createElement('div');
+    gridBox.classList.add('grid-box');
+    gridBox.style.minWidth = BOX_WIDTH;
+    gridBox.style.minHeight = BOX_WIDTH;
+    grid16.appendChild(gridBox);
 }
+
+
+let boxes = document.querySelectorAll('.grid-box');
+
+boxes.forEach(singleBox => {
+    
+    singleBox.addEventListener('mouseover', (event) => {
+
+        event.target.style.backgroundColor = 'aquamarine';
+    });
+
+    singleBox.addEventListener('mouseout', (event) => {
+
+        setTimeout(() => {
+            event.target.style.backgroundColor = 'transparent';
+          }, 500);
+    });
+});
