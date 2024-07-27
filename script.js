5d35338cd8bfada447b1f9801e54172a6553027e
JollyCoder <167334377+YoucefJC@users.noreply.github.com>
2024-07-27 15:41:01 +0200

    Add input possibility to change number of boxes
    Prompt the user for the number of boxes per side and replace the grid
    with the new generated grid.
    
    Still a problem with the displayed boxes as the number per side is not
    equal to the desired value.

--- script.js
+++ script.js
@@ -1,34 +1,55 @@
 const CONTAINER_WIDTH = '960';
 const NUM_BOX_PER_SIDE = 16;
-const NUM_BOXES = NUM_BOX_PER_SIDE ** 2;
-const BOX_WIDTH = Math.floor(CONTAINER_WIDTH / NUM_BOX_PER_SIDE) + 'px';
 
 let grid16 = document.querySelector('.grid-16-layout');
 grid16.style.maxWidth = CONTAINER_WIDTH + 'px';
 
-for (let Index = 0; Index < NUM_BOXES; Index++) {
-    
-    let gridBox = document.createElement('div');
-    gridBox.classList.add('grid-box');
-    gridBox.style.minWidth = BOX_WIDTH;
-    gridBox.style.minHeight = BOX_WIDTH;
-    grid16.appendChild(gridBox);
+setGrid(NUM_BOX_PER_SIDE);
+setTrail();
+
+function setGrid(numBoxesPerSide){
+
+    let numBoxes = numBoxesPerSide ** 2;
+    let boxWidth = Math.floor((CONTAINER_WIDTH / numBoxesPerSide)) + 'px';
+
+    let boxes = [];
+
+    for (let Index = 0; Index < numBoxes; Index++) {
+        
+        let gridBox = document.createElement('div');
+        gridBox.classList.add('grid-box');
+        gridBox.style.minWidth = boxWidth;
+        gridBox.style.minHeight = boxWidth;
+        boxes.push(gridBox);
+    }
+
+    grid16.replaceChildren(...boxes);
+    setTrail();
 }
 
-
-let boxes = document.querySelectorAll('.grid-box');
-
-boxes.forEach(singleBox => {
+function setTrail(){
     
-    singleBox.addEventListener('mouseover', (event) => {
+    let boxes = document.querySelectorAll('.grid-box');
 
-        event.target.style.backgroundColor = 'aquamarine';
+    boxes.forEach(singleBox => {
+        
+        singleBox.addEventListener('mouseover', (event) => {
+
+            event.target.style.backgroundColor = 'aquamarine';
+        });
+
+        singleBox.addEventListener('mouseout', (event) => {
+
+            setTimeout(() => {
+                event.target.style.backgroundColor = 'transparent';
+            }, 500);
+        });
     });
+}
 
-    singleBox.addEventListener('mouseout', (event) => {
+let changeBoxesBtn = document.querySelector('#change-boxes-btn');
+changeBoxesBtn.addEventListener('click', (event) => {
 
-        setTimeout(() => {
-            event.target.style.backgroundColor = 'transparent';
-          }, 500);
-    });
+    let numberOfBoxes = parseInt(prompt('Please input the number of boxes per side (max 100)'));
+    setGrid(numberOfBoxes);
 });
